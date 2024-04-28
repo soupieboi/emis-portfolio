ef4af7311e254dbd6f7fcdbc635b65ee9f9faf9d
mmispot <emiliadeboer1@gmail.com>
2024-04-29 00:31:59 +0200

    v1

--- emis-portfolio-emiwemi/main.js
+++ emis-portfolio-emiwemi/main.js
@@ -3,6 +3,25 @@ document.addEventListener("DOMContentLoaded", function (){
     const menu = document.querySelector(".dropdown-menu");
 
 // Spotify web playback SDK quickstart
+var client_id = 'CLIENT_ID';
+var redirect_uri = 'http://localhost:3000/callback';
+
+var app = express();
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  var scope = 'user-read-private user-read-email';
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
 
 
 // Mount an EventListener to the DOM that waits for a click on the burger
